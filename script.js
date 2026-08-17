87d1833cf7880f6c981382df6e7265262cbf10a9
baconburger080 <baconburger080@gmail.com>
2026-08-17 17:32:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const level1Screen =
     document.getElementById("level1-screen");
 
 
+/* ========================================= */
 /* START GAME */
+/* ========================================= */
 
 startButton.addEventListener("click", () => {
 
@@ -22,10 +24,79 @@ startButton.addEventListener("click", () => {
 });
 
 
+/* ========================================= */
 /* START AR */
+/* ========================================= */
 
-startARButton.addEventListener("click", () => {
+startARButton.addEventListener("click", async () => {
 
-    alert("AR MODE WILL START HERE");
+    try {
+
+        console.log("Requesting camera...");
+
+        const stream =
+            await navigator.mediaDevices.getUserMedia({
+
+                video: {
+                    facingMode: {
+                        ideal: "environment"
+                    }
+                },
+
+                audio: false
+
+            });
+
+
+        console.log("CAMERA ACCESS GRANTED");
+
+
+        /*
+         * สร้างหน้าจอกล้อง
+         */
+
+        const cameraScreen =
+            document.createElement("div");
+
+        cameraScreen.id = "camera-screen";
+
+
+        /*
+         * สร้างวิดีโอสำหรับแสดงภาพจากกล้อง
+         */
+
+        const video =
+            document.createElement("video");
+
+
+        video.autoplay = true;
+        video.playsInline = true;
+        video.muted = true;
+
+        video.srcObject = stream;
+
+
+        cameraScreen.appendChild(video);
+
+        document.body.appendChild(cameraScreen);
+
+
+        /*
+         * ซ่อนหน้า Level 1
+         */
+
+        level1Screen.classList.add("hidden");
+
+
+    } catch (error) {
+
+        console.error("CAMERA ERROR:", error);
+
+        alert(
+            "ไม่สามารถเปิดกล้องได้\n\n" +
+            error.message
+        );
+
+    }
 
 });
